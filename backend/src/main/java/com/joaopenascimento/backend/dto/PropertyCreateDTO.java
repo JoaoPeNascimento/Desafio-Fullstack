796dc3317a72f6ef0491d076dc434ff449e2491b
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2026-02-11 18:16:28 -0300

    backend: property DTOs

---
+++ backend/src/main/java/com/joaopenascimento/backend/dto/PropertyCreateDTO.java
@@ -0,0 +1,19 @@
+package com.joaopenascimento.backend.dto;
+
+import com.joaopenascimento.backend.model.enums.PropertyType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public record PropertyCreateDTO(
+        @NotBlank @Size(min = 10, max = 100) String name,
+        @NotBlank String description,
+        @NotNull PropertyType type,
+        @NotNull @Positive Double value,
+        @NotNull @Positive Integer bedrooms,
+        @NotBlank String address,
+        @NotBlank String city,
+        @NotBlank String state,
+        @NotNull Long brokerId
+) {}
---
+++ backend/src/main/java/com/joaopenascimento/backend/dto/PropertyDTO.java
@@ -0,0 +1,34 @@
+package com.joaopenascimento.backend.dto;
+
+import com.joaopenascimento.backend.model.Property;
+import com.joaopenascimento.backend.model.enums.PropertyType;
+
+public record PropertyDTO(
+        Long id,
+        String name,
+        String description,
+        PropertyType type,
+        Double value,
+        Integer bedrooms,
+        String address,
+        String city,
+        String state,
+        Long brokerId,
+        String brokerName
+) {
+    public PropertyDTO(Property entity) {
+        this(
+                entity.getId(),
+                entity.getName(),
+                entity.getDescription(),
+                entity.getType(),
+                entity.getValue(),
+                entity.getBedrooms(),
+                entity.getAddress(),
+                entity.getCity(),
+                entity.getState(),
+                entity.getBroker().getId(),
+                entity.getBroker().getName()
+        );
+    }
+}
