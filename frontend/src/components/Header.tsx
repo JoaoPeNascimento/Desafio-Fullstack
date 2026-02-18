87fc8cf15db01edb15aada5ca3e795f94d182428
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2026-02-17 21:29:00 -0300

    frontend: Header criado

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,98 @@
+import {
+  BuildingIcon,
+  HeartIcon,
+  LogOutIcon,
+  User2Icon,
+  UserCircleIcon,
+} from "lucide-react";
+import "./Header.css";
+import React from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import { useAuthStore } from "../store/useAuthStore";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const role = useAuthStore((state) => state.role);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleFavoritesPage = () => {
+    handleClose();
+    navigate("/favorites");
+  };
+
+  const handlePropertiesPage = () => {
+    handleClose();
+    navigate("/properties");
+  };
+
+  const handleUserPage = () => {
+    handleClose();
+    navigate("/user");
+  };
+
+  return (
+    <header className="App-header">
+      <div className="user-info">
+        <h1>IMOBILIARIA</h1>
+        <Button
+          id="user-btn"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <UserCircleIcon size={40} />
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleUserPage} divider className="menu-item">
+            <User2Icon size={16} className="menu-icon" />
+            Perfil
+          </MenuItem>
+          {role === "CLIENTE" && (
+            <MenuItem
+              onClick={handleFavoritesPage}
+              className="menu-item"
+              divider
+            >
+              <HeartIcon size={16} className="menu-icon" />
+              Meus favoritos
+            </MenuItem>
+          )}
+          {role !== "CLIENTE" && (
+            <MenuItem
+              onClick={handlePropertiesPage}
+              className="menu-item"
+              divider
+            >
+              <BuildingIcon size={16} className="menu-icon" />
+              Meus imóveis
+            </MenuItem>
+          )}
+          <MenuItem onClick={handleClose} className="menu-item">
+            <LogOutIcon size={16} className="logout-icon" />
+            Logout
+          </MenuItem>
+        </Menu>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
